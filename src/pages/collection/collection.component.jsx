24d6c3ffb361a45c3ef000ea1abba02cc8eb710b
Pereverziev Andrii <pereverziev.andrii@gmail.com>
2020-12-19 16:26:59 +0200

    Use CollectionsContext

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
-import { connect } from 'react-redux';
+
+import ColleciontsContext from '../../context/collections/collections.context';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
-import { selectCollection } from '../../redux/shop/shop.selectors';
-
 import './collection.styles.scss';
 
-const CollectionPage = ({ collection }) => {
-  const { title, items } = collection;
-  return (
-    <div className='collection-page'>
-      <h2 className='title'>{title}</h2>
-      <div className='items'>
-        {items.map(item => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-      </div>
-    </div>
-  );
-};
+const CollectionPage = ({ match }) => (
+  <ColleciontsContext.Consumer>
+    {
+      collections => {
+        const collection = collections[match.params.collectionId];
+        const { title, items } = collection;
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
+        return (
+          <div className='collection-page'>
+            <h2 className='title'>{title}</h2>
+            <div className='items'>
+              {items.map(item => (
+                <CollectionItem key={item.id} item={item} />
+              ))}
+            </div>
+          </div>
+        )
+      }
+    }
+  </ColleciontsContext.Consumer>
+);
 
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
