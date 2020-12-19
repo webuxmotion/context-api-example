431611d26ccbd5c5453c580a202344cf27890fbd
Pereverziev Andrii <pereverziev.andrii@gmail.com>
2020-12-19 18:50:49 +0200

    Replace Redux with ContextAPI. Not persisted cart

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React, { useContext } from 'react';
 
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
-import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
+import ColleciontsContext from '../../context/collections/collections.context';
 
 import './collections-overview.styles.scss';
 
-const CollectionsOverview = ({ collections }) => (
-  <div className='collections-overview'>
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
-  </div>
-);
+const CollectionsOverview = () => {
+  const collections = useContext(ColleciontsContext);
+  const collectionsForPreview = Object.keys(collections).map(key => collections[key]);
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollectionsForPreview
-});
+  return (
+    <div className='collections-overview'>
+      {collectionsForPreview.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  )
+};
 
-export default connect(mapStateToProps)(CollectionsOverview);
+export default CollectionsOverview;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-
-import { store, persistor } from './redux/store';
 
 import CartProvider from './providers/cart/cart.provider';
 
@@ -12,14 +8,10 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <CartProvider>
-      <BrowserRouter>
-        <PersistGate persistor={persistor}>
-          <App />
-        </PersistGate>
-      </BrowserRouter>
-    </CartProvider>
-  </Provider>,
+  <CartProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </CartProvider>,
   document.getElementById('root')
 );
